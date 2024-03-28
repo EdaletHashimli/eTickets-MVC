396309be192bf48def44c2ae42d48fe5c984a011
EdaletHashimli <edi.20.hashimli@gmail.com>
2024-03-28 16:44:50 +0400

    Adding EntityBaseRepository Files

---
+++ Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,32 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Actor> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Actor> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
